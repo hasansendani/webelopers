c90919d971add94e415eed94c299be195aeac616
hasansendani <hasansendani1@gmail.com>
2018-11-23 12:07:35 +0330

    salam omid

---
+++ mysite/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+from mysite.forms import SignUpForm
+
+
+def mainPage(request):
+    return render(request, 'mainPage.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            return redirect('main_page')
+    else:
+        form = SignUpForm()
+    return render(request, 'registerPage.html', {'form': form})
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hasansendani/PycharmProjects/webel/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
